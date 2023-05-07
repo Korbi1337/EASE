bcf462f3214e73a43e83e2f70a4cb2101dd21e2d
Korbi1337 <121751226+Korbi1337@users.noreply.github.com>
2023-05-07 17:23:58 +0200

    initial push

---
+++ 2023-04-19-Projekt-DMA.X/main.h
@@ -0,0 +1,24 @@
+// header file for main.c
+
+void initialize_HW(void);	// initialisation procedure
+
+
+
+void __attribute__((interrupt(auto_psv))) _DefaultInterrupt(void);
+void __attribute__((interrupt(auto_psv))) _T2Interrupt(void);
+void __attribute__((interrupt(auto_psv))) _CNInterrupt(void);
+void __attribute__((interrupt(auto_psv))) _T4Interrupt(void);
+
+
+
+volatile uint16_t timer4ms = 0;       //Globale Zählervariable Systemzeit
+volatile uint16_t delaytime4 = 0;     //Vergleichswert für Entprellzeit
+volatile uint16_t key_event=0;
+volatile uint16_t freqflag=1;
+volatile uint16_t soundflag=0;
+
+
+uint16_t entprellzeit=4;
+
+uint16_t Array[256]={1};
+uint16_t Array2[256]={0};
